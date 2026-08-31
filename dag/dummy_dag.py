9585d58f0343548066a6af81dc2a99c6ac21f5b0
Goel52 <goel52@yandex.ru>
2026-08-31 03:34:40 +0300

    f`1

---
+++ dag/dummy_dag.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+
+# Описание параметров текущего DAG 
+with DAG(
+        dag_id='dummy_dag',  # id DAG. Рекомендуем именовать DAG так же, как и сами файлы.
+        start_date=datetime(2023, 7, 13),  # Дата, с которой DAG начинает работу
+        schedule_interval='@daily',  # Расписание работы DAG
+        catchup=False,  # Параметр указывает, нужно ли запускать все DAG Run с момента start_date.
+) as dag:
+    dummy_task = EmptyOperator(  # Каждая задача, Task, является экземпляром оператора Airflow
+        task_id='dummy_task'  # id задачи
+    )
+
+    second_task = EmptyOperator(  # Каждая задача, Task, является экземпляром оператора Airflow
+        task_id='second_task'  # id задачи
+    )
+
+    third_task = EmptyOperator(  # Каждая задача, Task, является экземпляром оператора Airflow
+        task_id='third_task'  # id задачи
+    )
+
+    dummy_task >> second_task
+    dummy_task >> third_task 
