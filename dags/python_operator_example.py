d5f5d902fc9d1845b50ebcec3cda8023773e675f
Goel52 <goel52@yandex.ru>
2026-09-05 15:43:58 +0300

    123

--- dags/python_operator_example.py
+++ dags/python_operator_example.py
@@ -1,8 +1,7 @@
 from datetime import datetime, timedelta
 
 from airflow import DAG
-from airflow.operators.python import PythonOperator
-
+from airflow.operators.python import PythonOperator  # Импортируем оператор
 
 default_args = {
     'owner': 'airflow',
@@ -14,23 +13,21 @@ default_args = {
 
 def print_weekday(**kwargs):
     date = kwargs['date']
-    day = datetime.strptime(date, '%d.%m.%Y') 
+    day = datetime.strptime(date, '%d.%m.%Y')
     weekday = day.weekday()
     print('current weekday number is: ', weekday)
 
 
 with DAG('python_operator_example',
          default_args=default_args,
-         start_date=datetime(2024,1,1),
-         schedule_interval="@daily",
+         start_date=datetime(2024, 1, 1),
          max_active_runs=1,
-         catchup=True,
-         max_active_runs=1) as dag:
+         catchup=True) as dag:  # Создаём объект DAG
 
-    weekday_task = PythonOperator(
-        task_id='weekday_task',
-        python_callable=print_weekday, 
-        op_kwargs={'date':'01.01.2000'}
-    )
+    weekday_task = PythonOperator(task_id='weekday_task',
+                                  python_callable=print_weekday,
+                                  op_kwargs={'date': '01.01.2000'}
+                                  # Обратите внимание, что функция не вызывается, а передаётся
+                                  )
 
     weekday_task
