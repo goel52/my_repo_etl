0c592d2c01beb77a58d4ff8fd9f6ba3b34b89945
Goel52 <goel52@yandex.ru>
2026-09-05 15:42:34 +0300

    123

--- dags/python_operator_example.py
+++ dags/python_operator_example.py
@@ -1,82 +1,36 @@
-import json
 from datetime import datetime, timedelta
-from urllib.parse import urljoin
 
-import pydantic
-import requests
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+
 
 default_args = {
     'owner': 'airflow',
     'concurrency': 1,
-    'retries': 3,
-    'retry_delay': timedelta(seconds=10),
+    'retries': 2,
+    'retry_delay': timedelta(seconds=5),
 }
 
-API_host = 'https://de-start-sprint-etl-airflow-api.de.education-services.ru'
+
+def print_weekday(**kwargs):
+    date = kwargs['date']
+    day = datetime.strptime(date, '%d.%m.%Y') 
+    weekday = day.weekday()
+    print('current weekday number is: ', weekday)
 
 
-class UserOrderModel(pydantic.BaseModel):
-    id: int
-    uniq_id: str
-    date_time: datetime
-    city_id: int
-    city_name: str
-    customer_id: int
-    first_name: str
-    last_name: str
-    item_id: int
-    item_name: str
-    quantity: int
-    payment_amount: int
-
-
-class UserActivityModel(pydantic.BaseModel):
-    id: int
-    uniq_id: str
-    date_time: datetime
-    action_id: int
-    customer_id: int
-    quantity: int
-
-
-def parse_user_order_log():
-    payload = {'limit': '20000000', 'filter': {'date': '2024-09-19'}}
-    resp = requests.post(urljoin(API_host, 'user_order_log'), data=json.dumps(payload))
-    resp.raise_for_status()
-    msg = resp.json()
-    for row in msg:
-        respmodel = UserOrderModel(**row)
-        print(*respmodel.model_dump().values())
-
-
-def parse_user_activity_log():
-    payload = {'limit': '20000000', 'filter': {'date': '2024-09-19'}}
-    resp = requests.post(urljoin(API_host, 'user_activity_log'), data=json.dumps(payload))
-    resp.raise_for_status()
-    msg = resp.json()
-    for row in msg:
-        respmodel = UserActivityModel(**row)
-        print(*respmodel.model_dump().values())
-
-
-with DAG('api_data_load',
+with DAG('python_operator_example',
          default_args=default_args,
-         start_date=datetime(2024, 1, 1),
-         catchup=False,
-         schedule_interval='@once',
+         start_date=datetime(2024,1,1),
+         schedule_interval="@daily",
+         max_active_runs=1,
+         catchup=True,
          max_active_runs=1) as dag:
-    user_order_task = PythonOperator(
-        task_id='user_order_task',
-        python_callable=parse_user_order_log,
-        provide_context=True
+
+    weekday_task = PythonOperator(
+        task_id='weekday_task',
+        python_callable=print_weekday, 
+        op_kwargs={'date':'01.01.2000'}
     )
 
-    user_activity_task = PythonOperator(
-        task_id='user_activity_task',
-        python_callable=parse_user_activity_log,
-        provide_context=True
-    )
-
-    user_order_task >> user_activity_task
+    weekday_task
---
+++ dags/python_operator_example_old.py
@@ -0,0 +1,82 @@
+import json
+from datetime import datetime, timedelta
+from urllib.parse import urljoin
+
+import pydantic
+import requests
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+default_args = {
+    'owner': 'airflow',
+    'concurrency': 1,
+    'retries': 3,
+    'retry_delay': timedelta(seconds=10),
+}
+
+API_host = 'https://de-start-sprint-etl-airflow-api.de.education-services.ru'
+
+
+class UserOrderModel(pydantic.BaseModel):
+    id: int
+    uniq_id: str
+    date_time: datetime
+    city_id: int
+    city_name: str
+    customer_id: int
+    first_name: str
+    last_name: str
+    item_id: int
+    item_name: str
+    quantity: int
+    payment_amount: int
+
+
+class UserActivityModel(pydantic.BaseModel):
+    id: int
+    uniq_id: str
+    date_time: datetime
+    action_id: int
+    customer_id: int
+    quantity: int
+
+
+def parse_user_order_log():
+    payload = {'limit': '20000000', 'filter': {'date': '2024-09-19'}}
+    resp = requests.post(urljoin(API_host, 'user_order_log'), data=json.dumps(payload))
+    resp.raise_for_status()
+    msg = resp.json()
+    for row in msg:
+        respmodel = UserOrderModel(**row)
+        print(*respmodel.model_dump().values())
+
+
+def parse_user_activity_log():
+    payload = {'limit': '20000000', 'filter': {'date': '2024-09-19'}}
+    resp = requests.post(urljoin(API_host, 'user_activity_log'), data=json.dumps(payload))
+    resp.raise_for_status()
+    msg = resp.json()
+    for row in msg:
+        respmodel = UserActivityModel(**row)
+        print(*respmodel.model_dump().values())
+
+
+with DAG('api_data_load',
+         default_args=default_args,
+         start_date=datetime(2024, 1, 1),
+         catchup=False,
+         schedule_interval='@once',
+         max_active_runs=1) as dag:
+    user_order_task = PythonOperator(
+        task_id='user_order_task',
+        python_callable=parse_user_order_log,
+        provide_context=True
+    )
+
+    user_activity_task = PythonOperator(
+        task_id='user_activity_task',
+        python_callable=parse_user_activity_log,
+        provide_context=True
+    )
+
+    user_order_task >> user_activity_task
